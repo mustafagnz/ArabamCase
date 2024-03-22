795c79427b18282adebf87372d37c1906381215a
mustafagunduz <mustafagnz3478@gmail.com>
2024-03-22 14:05:16 +0300

    created utils folder and Constants.kt object.
    Add Constants.kt object for API endpoints
    
    - BASE_URL for the API base URL
    - ARABAM_LIST_END_POINT for listing endpoint
    - ARABAM_DETAIL_END_POINT for car detail endpoint

---
+++ app/src/main/java/com/example/arabamcase/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.arabamcase.utils
+
+object Constants {
+    const val BASE_URL : String = "http://sandbox.arabamd.com/api/v1/"
+    const val ARABAM_LIST_END_POINT : String = "listing?sort=1&sortDirection=0&take=10"
+    const val ARABAM_DETAIL_END_POINT : String = "detail?id={arabamId}"
+
+}
